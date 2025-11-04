12b1c75e80c5bedc57e0c404f62d5f2c297598c2
Sarumathi Sugumar <sarumathi.sugumar@aspiresys.com>
2025-11-05 00:30:55 +0530

    Implemented new feature

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { loadRemoteModule } from '@angular-architects/module-federation';
 
-const routes: Routes = [];
+const routes: Routes = [ {
+    path: 'debt',
+    loadChildren: () =>
+      loadRemoteModule({
+        type: 'module',
+        remoteEntry: 'http://localhost:4300/remoteEntry.js',
+        exposedModule: './Module',
+      }).then((m: { RemoteEntryModule: any; }) => m.RemoteEntryModule),
+  },];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

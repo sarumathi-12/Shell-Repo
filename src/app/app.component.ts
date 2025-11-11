e24dcc528ef353f84052fdab50722f750360998f
Sarumathi Sugumar <sarumathi.sugumar@aspiresys.com>
2025-11-11 23:09:43 +0530

    Addressed CSS PR comments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,54 +1,52 @@
-import { Component, TemplateRef, ViewChild } from '@angular/core';
+import { Component, TemplateRef, ViewChild, ViewEncapsulation } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { NavigationEnd, Router } from '@angular/router';
 import { filter } from 'rxjs';
 import { AuthService } from './services/auth.service';
 import { Overlay } from '@angular/cdk/overlay';
+import { DEFAULT_ENCAPSULATION } from './_shared/constants/debt.const';
 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  encapsulation: DEFAULT_ENCAPSULATION
 })
 export class AppComponent {
-  showMenu = false; 
 
   @ViewChild('logoutDialog') logoutDialog!: TemplateRef<any>;
 
   constructor(private dialog: MatDialog, private router: Router, private authService: AuthService, private overlay: Overlay) { }
 
-  get isLoginPage(): boolean {
+  public get isLoginPage(): boolean {
     return this.router.url.includes('login');
   }
 
-  toggleMenu(): void {
-    this.showMenu = !this.showMenu;
-  }
 
-  openLogoutDialog() {
-    this.showMenu = false;
-    const dialogRef = this.dialog.open(this.logoutDialog, {
-      width: '320px',
-      disableClose: true,
-      panelClass: 'logout-dialog-panel',
-    });
+public openLogoutDialog() {
+  const dialogRef = this.dialog.open(this.logoutDialog, {
+    width: '320px',
+    disableClose: true,
+    panelClass: 'logout-dialog-panel'
+  });
 
-    dialogRef.afterClosed().subscribe(result => {
-      if (result) this.logout();
-    });
-  }
+  dialogRef.afterClosed().subscribe(result => {
+    if (result) this.logout();
+  });
+}
 
-  closeDialog(): void {
+
+  public closeDialog(): void {
     this.dialog.closeAll();
   }
 
-  confirmLogout(): void {
+  public confirmLogout(): void {
     this.dialog.closeAll();
     this.logout();
   }
 
-  logout(): void {
+  public logout(): void {
     this.authService.logout(); 
     this.router.navigate(['/login']);
   }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,7 +16,6 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatDividerModule } from '@angular/material/divider';
-import { OverlayModule } from '@angular/cdk/overlay';
 
 
 
@@ -38,8 +37,8 @@ import { OverlayModule } from '@angular/cdk/overlay';
     MatMenuModule,
     MatDialogModule,
     MatDividerModule,
-    OverlayModule,
-    ToastrModule.forRoot(), ],
+    ToastrModule.forRoot(),
+  MatButtonModule ],
   providers: [],
   bootstrap: [AppComponent]
 })
